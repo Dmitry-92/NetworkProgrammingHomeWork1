237c454df0e1f7e66584917de715a66fa930efb0
Дмитрий М. <s1ash@mail.ru>
2026-05-24 14:05:51 +0300

    Шаг 4: Сервер принимает объект Book через TCP и десериализует JSON

--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using SharedModels;
+
+Console.WriteLine("Сервер запущен!");
+
+TcpListener listener = new TcpListener(IPAddress.Any, 8888);
+listener.Start();
+Console.WriteLine("Ожидание подключения клиента...");
+
+TcpClient client = await listener.AcceptTcpClientAsync();
+Console.WriteLine("Клиент подключен.");
+
+NetworkStream stream = client.GetStream();
+byte[] buffer = new byte[4096];
+int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+string json = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+Book? receivedBook = JsonSerializer.Deserialize<Book>(json);
+if (receivedBook != null)
+{
+    Console.WriteLine($"Получена книга: {receivedBook}");
+}
+else
+{
+    Console.WriteLine("Ошибка десериализации");
+}
+
+client.Close();
+listener.Stop();
+Console.WriteLine("Сервер остановлен");
